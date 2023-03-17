f041d0f07d9f8504f7fdbb2ba7c5597612bea11e
zin <zineb.abidi.ma@gmail.com>
2023-03-17 15:20:44 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 /*
- * the main c function
+ * main -Entry point
  *
- * return **/
+ * DEscription: A C program that prints with put function 
+ *
+ * Return: Alawys 0 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
