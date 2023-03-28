dd97b6ca73205689d9ab72ee83bc90ccbd206ba3
zin <zineb.abidi.ma@gmail.com>
2023-03-28 10:18:53 -0700

    a

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: input
+ * @src: input
+ * Return: the pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (scr[i] != '\0');
+
+	return (dest);
+}
