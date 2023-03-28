8d63a8dd26a22839cffe1a6d07f6a123baf6a743
zin <zineb.abidi.ma@gmail.com>
2023-03-28 09:36:17 -0700

    a

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: input
+ * Return: null
+*/
+
+void print_rev(char *s)
+{
+	int  i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
