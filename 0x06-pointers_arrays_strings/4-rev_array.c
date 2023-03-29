79b45b818e31db4c66f28f728cecc6f55c7340bc
zin <zineb.abidi.ma@gmail.com>
2023-03-29 15:50:28 -0700

    a

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,3 +6,15 @@
  * @n: input
  * Return: 0 Success
 */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, t;
+
+	for (i = 0, j = (n - 1); i < j; i++, j--)
+	{
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
