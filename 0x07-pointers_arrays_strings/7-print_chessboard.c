3c55ef9bec30c85e50d5038fb9e7aad2e5b662e2
zin <zineb.abidi.ma@gmail.com>
2023-04-04 17:15:35 -0700

    a

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,23 +1,22 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_chessboard - prints the chessboard
- * @a: input array rows
+ * @a: input
  * Return: 0
 */
 
 void print_chessboard(char (*a)[8])
 {
-	int c, b;
+	int i, j;
 
-	for (c = 0; c < 8; c++)
+	for (i = 0; i < 8; i++)
 	{
-		for (b = 0; b < 8; b++)
+		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[c][b]);
+			printf("%d", a[i][j]);
 		}
-
-		_putchar('\n');
+		printf("\n");
 	}
 
 }
