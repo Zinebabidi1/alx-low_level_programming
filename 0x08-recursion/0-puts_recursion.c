6cbcf4d76c4a5e659cdc06e55be997f34119bf9b
zin <zineb.abidi.ma@gmail.com>
2023-04-05 14:02:35 -0700

    c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,17 +3,19 @@
 /**
  * _puts_recursion -  prints a string, followed by a new line.
  * @s: input
- * Return: the string
+ * Return: null
 */
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
+	{
 		_putchar('\n');
+	}
 	else
 	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 
 }
