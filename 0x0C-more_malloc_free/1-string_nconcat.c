fcd7369ffd36980d20132c7e96b7c343e749b900
zin <zineb.abidi.ma@gmail.com>
2023-04-13 16:06:26 -0700

    a

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: input
+ * @s2: input
+ * @n: input
+ * Return: pointer shall point to a newly allocated space in memory,
+ * which contains s1,
+ * followed by the first n bytes of s2, and null terminated
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+}
