b996bbc4d74eac9c7996fe99aad9e7fac9ef6a38
zin <zineb.abidi.ma@gmail.com>
2023-04-14 16:59:54 -0700

    a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -32,12 +32,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-
 	m = malloc(sizeof(int) * nmemb);
-
 	if (m == 0)
 		return (NULL);
-
 	_memset(m, 0, sizeof(int) * nmemb);
 
 	return (m);
