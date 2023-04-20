f3caaf3387e792767c6ed63044b58f17b6b861c9
zin <zineb.abidi.ma@gmail.com>
2023-04-20 15:59:05 -0700

    a

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: args
+ * @argv: vector
+ * Return: always 0
+*/
+
+int main(int argc, char **argv)
+{
+	char *p = (char *)main;
+	int b;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
+
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	return (0);
+}
