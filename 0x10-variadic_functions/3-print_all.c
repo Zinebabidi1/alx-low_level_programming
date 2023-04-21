a5db6beabbf194b1ca77cabc1a9590e946985ffc
zin <zineb.abidi.ma@gmail.com>
2023-04-20 18:56:14 -0700

    a

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,91 @@
+#include "variadic_functions.h"
+
+/**
+ * f_char - formats char
+ * @separator: char
+ * @ap: pointer
+*/
+
+void f_char(char *separator, va_list ap)
+{
+	printf("%s%c", separator, va_arg(ap, int));
+}
+
+/**
+ * f_int - formats int
+ * @separator: char
+ * @ap: pointer
+*/
+
+void f_int(char *separator, va_list ap)
+{
+	printf("%s%d", separator, va_arg(ap, int));
+}
+
+/**
+ * f_float - formats float
+ * @separator: char
+ * @ap: pointer
+*/
+
+void f_float(char *separator, va_list ap)
+{
+	printf("%s%d", separator, va_arg(ap, double));
+}
+
+/**
+ * f_string - formats string
+ * @separator: char
+ * @ap: pointer
+*/
+
+void f_string(char *separator, va_list ap)
+{
+	char *str = va_arg(ap, char *);
+
+	switch ((int)(!str))
+		case 1:
+			str = "(nil)";
+
+	printf("%s%s", separator, str);
+}
+
+/**
+ * print_all - prints anything.
+ * @format: const
+ * @...: int
+ * Return: void
+*/
+
+void print_all(const char * const format, ...)
+{
+	int i = 0, j;
+	char *separator = "";
+	va_list ap;
+	token_t tokens[] = {
+		{"c", f_char},
+		{"i", f_int},
+		{"f", f_float},
+		{"s", f_string},
+		{NULL, NULL}
+
+	};
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (tokens[j].token)
+		{
+			if (format[i] == tokens[j].token[0])
+			{
+				tokens[j].f(separator, ap);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
