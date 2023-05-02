3935b9e261b99c79b0cb07b621825c450e9bc626
partner <zineb.abidi.ma@gmail.com>
2023-05-02 16:06:30 -0700

    a

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,7 +2,28 @@
 #include <stdlib.h>
 
 /**
- * */
+ * find - finds a loop
+ * @head: liked list to search
+ * Return: @ of node
+*/
+
+listint_t *find(listint_t *head)
+{
+	listint_t *ptr, *end;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
+	}
+	return (NULL);
+}
 
 
 /**
@@ -13,4 +34,32 @@
 
 size_t free_listint_safe(listint_t **h)
 {
+	listint_t *next, *loopnode;
+	size_t len;
+	int loop = 1;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	loopnode = find(*h);
+	for (len = 0; (*h != loopnode || loop) && *h != NULL; *h = next)
+	{
+		len++;
+		next = (*h)->next;
+		if (*h == loopnode && loop)
+		{
+			if (loopnode == loopnode->next)
+			{
+				free(*h);
+				break;
+			}
+			len++;
+			next = next->next;
+			free((*h)->next);
+			loop = 0;
+		}
+		free(*h);
+	}
+	*h = NULL;
+	return (len);
 }
